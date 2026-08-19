2ffdac50600c2e476a7f6335448ceee0642ce0a0
viusa <viusansantos@gmail.com>
2026-08-19 16:36:43 +0200

    sucessfully created server that runs

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const Database = require('better-sqlite3');
+const express = require('express');
+
+const app = express()
 
 // Open database, it will create a file is it does not exist
 const db = new Database('trackerlog.db');
@@ -22,3 +25,21 @@ db.exec(`
        notes TEXT
     )
 `);
+
+// Middleware that allows express to understand JSON req bodies, and serve html/css/js
+app.use(express.json());
+app.use(express.static('public'));
+
+// Route: create a new problem entry
+app.post('/api/problems', (req, res) => {
+    const { name, difficulty, notes } = req.body;   
+
+    const stmt = db.prepare('INSERT INTO problems (name, difficulty, notes) VALUES (?, ?, ?)');
+    const info = stmt.run(name, difficulty, notes);
+ 
+    res.status(201).json({ id: info.lastInsertRowid, name, difficulty, notes });
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
