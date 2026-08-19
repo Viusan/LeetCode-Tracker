58d2af9c93c23150ff927c67c612c2ee9e8af852
viusa <viusansantos@gmail.com>
2026-08-19 16:21:32 +0200

    comments on app.js

--- public/app.js
+++ public/app.js
@@ -6,5 +6,13 @@ document.getElementById('progressForm').addEventListener('submit', async (e) => 
     const description = document.getElementById('description').value;
     const difficulty = document.getElementById('difficulty').value;
 
-    
+    // Making HTTP req
+    const res = await fetch('/api/problems', {
+        method: 'POST', // POST Method cause we creating
+        headers: { 'Content-Type': 'application/json' }, // Label telling we are sending in JSON 
+        body: JSON.stringify({ name, difficulty, notes: description }), // Actual data we are sending in JSON formatted string
+    });
+
+    const saved = await res.json(); // Raw HTTP response
+    console.log('Saved:', saved); // Print just to see the response and visually confirm if we want to
 });
